8c4a04cf8a477aa5d1a1b50306d3732e4b563052
Daniel Bradley <daniel@crossadaptive.com>
2023-02-18 16:15:46 +1000

    Redacted includes in PSAs to strictly necessary.

--- source/src/libbase/Darwin/IO_sendFile.c
+++ source/src/libbase/Darwin/IO_sendFile.c
@@ -1,17 +1,6 @@
 #include "libbase.h"
 
-#include <errno.h>
-#include <fcntl.h>
-#include <libgen.h>
 #include <netinet/ip.h>
-#include <pwd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <uuid/uuid.h>
 
 struct _IO
 {
