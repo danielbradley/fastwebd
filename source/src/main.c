371d608e9a691b32f59fa2654e704d12719e34de
Daniel Bradley <daniel@crossadaptive.com>
2023-03-04 21:25:14 +1000

    Changes to main:
    +   Modified main - now calls 'SysLog_Start'.
    +   Now processes arguments to retrieve optional arguments '--port' and '--default-domain'.

--- source/src/main.c
+++ source/src/main.c
@@ -1,35 +1,60 @@
-
 #include <stdlib.h>
 #include "libhttpserver.h"
 
 int main( int argc, char** argv )
 {
-    int port = 8080;
-    
-    if ( argc > 1 )
+    SysLog_Start();
+
+    SysLog_Log_chars( "Started " );
     {
-        port = atoi( argv[1] );
-        
-        if ( !port ) port = 8080;
+        String* default_domain = null;
+        int     port           = 8080;
+
+        for ( int i=1; i < argc; i++ )
+        {
+            String* arg = String_new( argv[i] );
+            if ( String_contentEquals( arg, "--default-domain" ) )
+            {
+                if ( (i+1) < argc )
+                {
+                    default_domain = String_new( argv[i+1] );
+                    i++;
+                }
+            }
+            else
+            if ( String_contentEquals( arg, "--port" ) )
+            {
+                if ( (i+1) < argc )
+                {
+                    port = atoi( argv[i+1] );
+                    i++;
+                }
+            }
+            String_free( &arg );
+        }
+
+        HTTPServer* server = HTTPServer_new_port( port );
+
+        if ( !server )
+        {
+            HTTPServer_Panic();
+        }
+        else
+        if ( !HTTPServer_listen( server ) )
+        {
+            HTTPServer_InvalidPort();
+        }
+        else
+        {
+            HTTPServer_setDefaultDomain ( server, default_domain );
+            HTTPServer_acceptConnections( server               );
+        }
+
+        HTTPServer_free( &server );
+        String_free( &default_domain );
     }
 
-    HTTPServer* server = HTTPServer_new_port( port );
-
-    if ( !server )
-    {
-        HTTPServer_Panic();
-    }
-    else
-    if ( !HTTPServer_listen( server ) )
-    {
-        HTTPServer_InvalidPort();
-    }
-    else
-    {
-        HTTPServer_acceptConnections( server );
-    }
-
-    HTTPServer_free( &server );
+    SysLog_Stop();
 
     Exit( 0 );
 }
